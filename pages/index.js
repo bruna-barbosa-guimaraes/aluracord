db0d723e9a42070ba222d015a0a6c137ded5db6c
Bruna Guimarães <62905224+brunaguimaraesssss@users.noreply.github.com>
2022-01-25 22:01:12 -0300

    Aula 2

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,7 @@
 import appConfig from '../config.json';
+import React from 'react';
+import { useRouter } from 'next/router';
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
-
-function GlobalStyle() {
-    return (
-        <style global jsx>{`
-        * {
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-            list-style: none;
-        }
-        body {
-            font-family: 'Open Sans', sans-serif;
-        }
-        /* App fit Height */ 
-        html, body, #__next {
-            min-height: 100vh;
-            display: flex;
-            flex: 1;
-        }
-        #__next {
-            flex: 1;
-        }
-        #__next > * {
-            flex: 1;
-        }
-        /* ./App fit Height */ 
-        `}</style>
-    )
-}
 
 function Title(props) {
     const Tag = props.tag || 'h1';
@@ -39,6 +12,7 @@ function Title(props) {
                 ${Tag} {
                     color: ${appConfig.theme.colors.primary['500']};
                     font-size: 24px;
+                    padding: 10px;
                 }
             `}</style>
         </>
@@ -58,11 +32,13 @@ function Title(props) {
 // export default HomePage
 
 export default function PaginaInicial() {
-    const username = 'brunaguimaraesssss';
+    // const username = 'brunaguimaraesssss';
+    // hooks
+    const [username, setUsername] = React.useState('');
+    const router = useRouter();
 
     return (
         <>
-            <GlobalStyle />
             <Box
                 styleSheet={{
                     display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -82,24 +58,50 @@ export default function PaginaInicial() {
                         },
                         width: '100%', maxWidth: '700px',
                         borderRadius: '5px', padding: '32px', margin: '16px',
-                        boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
-                        backgroundColor: appConfig.theme.colors.neutrals[700],
+                        boxShadow: '0 0 2em 0 black',
+                        backgroundColor: appConfig.theme.colors.neutrals[500],
+                        opacity: 0.8
                     }}
                 >
                     {/* Formulário */}
                     <Box
                         as="form"
+                        value="{username}"
+                        // evita submit vazio e não recarrega a página
+                        onSubmit={function (event) {
+                            event.preventDefault();
+                            router.push('/chat');
+                            }
+                        }
                         styleSheet={{
                             display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                             width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
                         }}
                     >
                         <Title tag="h2">Boas vindas de volta!</Title>
-                        <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
+                        <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[200] }}>
                             {appConfig.name}
                         </Text>
 
+                        {/* <input 
+                            type="text"
+                            value={username}
+                            onChange={function onHandler(event) {
+                                // onde está o valor
+                                const valor = event.target.value;
+                                // trocar o valor da variável
+                                setUsername(valor);
+                            }}
+                        /> */}
+
                         <TextField
+                            value={username}
+                            onChange={function onHandler(event) {
+                                // onde está o valor
+                                const valor = event.target.value;
+                                // trocar o valor da variável
+                                setUsername(valor);
+                            }}
                             fullWidth
                             textFieldColors={{
                                 neutral: {
@@ -146,7 +148,7 @@ export default function PaginaInicial() {
                                 borderRadius: '50%',
                                 marginBottom: '16px',
                             }}
-                            src={`https://github.com/${username}.png`}
+                                src={`https://github.com/${username}.png`}
                         />
                         <Text
                             variant="body4"
